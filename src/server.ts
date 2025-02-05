b6a0c11ef37221f3e0bd42274a5a0e6fc6f7af1c
maydayoWS <62242669+maydayoWS@users.noreply.github.com>
2025-02-05 14:27:58 +0700

    change port number

--- src/server.ts
+++ src/server.ts
@@ -17,5 +17,5 @@ app.get("/", (req: Request, res: Response) => {
   res.status(200).json({ message: "Hello World" });
 });
 
-const PORT = process.env.PORT || 3005;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
