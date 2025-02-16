08f20696a151bd69543b5626ad92d7f020a1cd02
Bulagorn Sasanthei <bulagorn4811@gmail.com>
2025-02-17 01:33:15 +0700

    add logger middleware to user service

--- user/src/server.ts
+++ user/src/server.ts
@@ -1,10 +1,12 @@
 import express, { Request, Response } from "express";
 import dotenv from "dotenv";
 import userRoutes from "./infrastructure/routes/user.route";
+import { accessLogger } from "./infrastructure/middlewares/logger";
 
 dotenv.config();
 const app = express();
 
+app.use(accessLogger);
 app.use(express.json());
 
 app.use("/v1/users", userRoutes);
