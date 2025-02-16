86c9faa071761aeef3f49d05c86f4a9fc34f7fc9
Bulagorn Sasanthei <bulagorn4811@gmail.com>
2025-02-16 16:43:00 +0700

    update user service with unit test

--- user/src/usecases/register.usecase.ts
+++ user/src/usecases/register.usecase.ts
@@ -1,7 +1,6 @@
 import { v4 as uuidv4 } from "uuid";
 
 import { UserRepositoryInterface } from "../domain/repositories/userRepository.interface";
-import { Request, Response } from "express";
 
 export class RegisterUsecase {
 
@@ -10,17 +9,17 @@ export class RegisterUsecase {
   execute = (email: string, password: string) => {
 
     if (!email || !password) {
-      throw new Error("Email and password are required")
+      throw new Error("Email and password are required");
     }
 
     if (!email.includes("@")) {
-      throw new Error("Invalid email format")
+      throw new Error("Invalid email format");
     }
 
 
     const existingUser = this.userRepository.findOneByEmail(email);
     if (existingUser) {
-      throw new Error("User already exists")
+      throw new Error("User already exists");
     }
 
     const newUser = {
@@ -35,12 +34,12 @@ export class RegisterUsecase {
       role: "user" as const,
     };
 
-    this.userRepository.createOne(newUser)
+    this.userRepository.createOne(newUser);
 
     console.log(`Sending welcome email to ${email}`);
 
-    return newUser
-  }
+    return newUser;
+  };
 
 
 }
